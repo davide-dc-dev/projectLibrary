71385a62e2c94d65aad770671956e8e10e328be8
Giuseppe De Vivo <E-giuseppe.devivo@msc.com>
2023-02-01 12:28:10 +0100

    modifica classe Book commento

--- projectLibrary/projectLibrary.Core/Entities/Book.cs
+++ projectLibrary/projectLibrary.Core/Entities/Book.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace projectLibrary.Core.Entities
 {
-    internal class Book
+    public class Book
     {
+        public int ISBN { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        
+        // nde pacc
+        public Book (int iSBN, string title, string author, string genre )
+        {
+            ISBN = iSBN;
+            Title = title;
+            Author = author;
+            Genre = genre;
+        }
     }
 }
